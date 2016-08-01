19c319fd7507bba85bb44bca972633980dae6bf5
josaint13 <josaint13@gmail.com>
2016-08-02 00:54:27 +0300

    Vowel shift

--- CodeWars/StringKata.cs
+++ CodeWars/StringKata.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CodeWars
@@ -9,6 +10,48 @@ namespace CodeWars
         {
             var res = s.GroupBy(x => x.ToString(), x=>x, StringComparer.OrdinalIgnoreCase).FirstOrDefault(g => g.Count() == 1);
             return res == null ? "" : res.Key;
+        }
+
+        public static string VowelShifting(string text, int n)
+        {
+            if (string.IsNullOrEmpty(text) || n == 0)
+            {
+                return text;
+            }
+
+            var vowels  = new List<char>{ 'a','e','i','o','u' };
+            var dict = new Dictionary<int, char>();
+
+            var source = text.ToArray();
+
+            for (var i = 0; i < source.Length; i++)
+            {
+                if (vowels.Contains(text[i]))
+                {
+                   dict.Add(i, text[i]);
+                }
+            }
+
+            var keys = dict.Keys.ToArray();
+            var elementIndex = 0;
+            foreach (var key in dict.Keys)
+            {
+                var index = elementIndex;
+
+                for (var j = 0; j < n; j++)
+                {
+                    if (++index > keys.Length-1)
+                    {
+                        index = 0;
+                    }
+                }
+
+                var targetIndex = keys[index];
+                source[targetIndex] = dict[key];
+                elementIndex ++;
+            }
+
+            return new string(source);
         }
     }
 }
--- CodeWarsTests/StringKataTests.cs
+++ CodeWarsTests/StringKataTests.cs
@@ -1,16 +1,37 @@
-﻿using CodeWars;
+﻿using System;
+using CodeWars;
 using Xunit;
+using Xunit.Abstractions;
 
-namespace CodeWarTests
+namespace CodeWarsTests
 {
     public class StringKataTests
     {
+        private readonly ITestOutputHelper _output;
+
+        public StringKataTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         [Fact]
         public void ShouldReturnFirstNonRepeatedLetter()
         {
             Assert.Equal("a", StringKata.FirstNonRepeatingLetter("a"));
             Assert.Equal("t", StringKata.FirstNonRepeatingLetter("stress"));
             Assert.Equal("e", StringKata.FirstNonRepeatingLetter("moonmen"));
+        }
+
+        [Fact]
+        public void ShoulShiftVowel()
+        {
+            Assert.Equal("This is a test!", StringKata.VowelShifting("This is a test!", 0));
+
+            _output.WriteLine("Thes is i tast!");
+            var actual1 = StringKata.VowelShifting("This is a test!", 1);
+            _output.WriteLine(actual1);
+            Assert.Equal("Thes is i tast!", actual1);
+            Assert.Equal("This as e tist!", StringKata.VowelShifting("This is a test!", 3));
         }
     }
 }
